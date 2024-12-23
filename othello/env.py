0b8802a45c26c4fb08fcc97f250a98f7b5a53810
edegp <a.yuhi1164@gmail.com>
2024-12-23 22:28:07 +0900

    remove render

--- othello/env.py
+++ othello/env.py
@@ -4,9 +4,6 @@ import os
 
 import numpy as np
 import numpy.typing as npt
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
-from IPython.core.pylabtools import print_figure
 
 # isort: off
 from othello.libcpp import CEnv
@@ -19,58 +16,58 @@ __all__ = ["Env", "make"]
 
 
 class Env(CEnv):
-    def render(self) -> npt.NDArray[np.uint8]:
-        size = 1024  # size of board
-        csize = size // 8  # size of cell
-        margin = 10  # margin from disk to grid line
+    # def render(self) -> npt.NDArray[np.uint8]:
+    #     size = 1024  # size of board
+    #     csize = size // 8  # size of cell
+    #     margin = 10  # margin from disk to grid line
 
-        # Initialize board with green color
-        img = Image.new("RGB", (size, size), color="#007700")
-        draw = ImageDraw.Draw(img)
+    #     # Initialize board with green color
+    #     img = Image.new("RGB", (size, size), color="#007700")
+    #     draw = ImageDraw.Draw(img)
 
-        # If board history exists, highlight last move
-        if self.history_size > 0:
-            last_action = self.last_action
-            i, j = last_action.x, last_action.y
-            if 0 <= i < 8 and 0 <= j < 8:
-                x = i * csize
-                y = j * csize
-                draw.rectangle((x, y, x + csize, y + csize), fill="#00aa00")
+    #     # If board history exists, highlight last move
+    #     if self.history_size > 0:
+    #         last_action = self.last_action
+    #         i, j = last_action.x, last_action.y
+    #         if 0 <= i < 8 and 0 <= j < 8:
+    #             x = i * csize
+    #             y = j * csize
+    #             draw.rectangle((x, y, x + csize, y + csize), fill="#00aa00")
 
-        # Grid lines
-        for i in range(1, 8):
-            y = i * csize
-            draw.line((0, y, size, y), "black", width=3)
+    #     # Grid lines
+    #     for i in range(1, 8):
+    #         y = i * csize
+    #         draw.line((0, y, size, y), "black", width=3)
 
-        for j in range(1, 8):
-            x = j * csize
-            draw.line((x, 0, x, size), "black", width=2)
+    #     for j in range(1, 8):
+    #         x = j * csize
+    #         draw.line((x, 0, x, size), "black", width=2)
 
-        # Stars
-        stars = [
-            (csize * 2 - 10, csize * 2 - 10, csize * 2 + 10, csize * 2 + 10),
-            (csize * 2 - 10, csize * 6 - 10, csize * 2 + 10, csize * 6 + 10),
-            (csize * 6 - 10, csize * 2 - 10, csize * 6 + 10, csize * 2 + 10),
-            (csize * 6 - 10, csize * 6 - 10, csize * 6 + 10, csize * 6 + 10),
-        ]
-        for st in stars:
-            draw.ellipse(st, fill="black")
+    #     # Stars
+    #     stars = [
+    #         (csize * 2 - 10, csize * 2 - 10, csize * 2 + 10, csize * 2 + 10),
+    #         (csize * 2 - 10, csize * 6 - 10, csize * 2 + 10, csize * 6 + 10),
+    #         (csize * 6 - 10, csize * 2 - 10, csize * 6 + 10, csize * 2 + 10),
+    #         (csize * 6 - 10, csize * 6 - 10, csize * 6 + 10, csize * 6 + 10),
+    #     ]
+    #     for st in stars:
+    #         draw.ellipse(st, fill="black")
 
-        # Disks
-        B = (self.board == +1).astype("int32")
-        W = (self.board == -1).astype("int32")
-        for i in range(8):
-            for j in range(8):
-                px = j * csize + margin
-                py = i * csize + margin
-                diameter = csize - margin * 2
-                xy = (px, py, px + diameter, py + diameter)
-                if B[i, j] != 0:
-                    draw.ellipse(xy, fill="black")
-                if W[i, j] != 0:
-                    draw.ellipse(xy, fill="white")
+    #     # Disks
+    #     B = (self.board == +1).astype("int32")
+    #     W = (self.board == -1).astype("int32")
+    #     for i in range(8):
+    #         for j in range(8):
+    #             px = j * csize + margin
+    #             py = i * csize + margin
+    #             diameter = csize - margin * 2
+    #             xy = (px, py, px + diameter, py + diameter)
+    #             if B[i, j] != 0:
+    #                 draw.ellipse(xy, fill="black")
+    #             if W[i, j] != 0:
+    #                 draw.ellipse(xy, fill="white")
 
-        return np.array(img, dtype="uint8")
+    #     return np.array(img, dtype="uint8")
 
     def __repr__(self) -> str:
         B = (self.board == +1).astype("int32")
@@ -107,23 +104,23 @@ class Env(CEnv):
             "is_lose",
         ]
 
-    def _repr_png_(self):
-        fig, ax = plt.subplots(figsize=(6, 6), dpi=100)
-        img = self.render()
-        size = img.shape[0]
-        csize = size // 8
+    # def _repr_png_(self):
+    #     fig, ax = plt.subplots(figsize=(6, 6), dpi=100)
+    #     img = self.render()
+    #     size = img.shape[0]
+    #     csize = size // 8
 
-        ax.imshow(img)
-        ax.xaxis.tick_top()
-        ax.set(
-            xticks=np.arange(csize // 2, size, csize),
-            yticks=np.arange(csize // 2, size, csize),
-            xticklabels=[chr(ord("A") + i) for i in range(8)],
-            yticklabels=np.arange(1, 9),
-        )
-        data = print_figure(fig, "png")
-        plt.close(fig)
-        return data
+    #     ax.imshow(img)
+    #     ax.xaxis.tick_top()
+    #     ax.set(
+    #         xticks=np.arange(csize // 2, size, csize),
+    #         yticks=np.arange(csize // 2, size, csize),
+    #         xticklabels=[chr(ord("A") + i) for i in range(8)],
+    #         yticklabels=np.arange(1, 9),
+    #     )
+    #     data = print_figure(fig, "png")
+    #     plt.close(fig)
+    #     return data
 
 
 def make() -> Env:
